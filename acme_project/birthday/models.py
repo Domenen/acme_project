e246f87ec0f92aa78893b24c019a2b1ccacca756
Sergey Beschstniy <Diman.9533@gmail.com>
2024-05-20 17:49:59 +0300

    add image

--- acme_project/birthday/models.py
+++ acme_project/birthday/models.py
@@ -1,1 +1,21 @@
 from django.db import models
+
+from .validators import real_age
+
+
+class Birthday(models.Model):
+    """Модель базы данных."""
+
+    first_name = models.CharField('Имя', max_length=20)
+    last_name = models.CharField(
+        'Фамилия', blank=True, help_text='Необязательное поле', max_length=20
+    )
+    birthday = models.DateField('Дата рождения', validators=(real_age,))
+    image = models.ImageField('Фото', upload_to='birthdays_images', blank=True)
+
+    constraints = (
+            models.UniqueConstraint(
+                fields=('first_name', 'last_name', 'birthday'),
+                name='Unique person constraint',
+            ),
+        )
